611c9b55e2aac21cc86ada8e0accdef9acf87544
SmolIndieGame <smolindiegame@gmail.com>
2023-11-11 17:08:39 +0800

    ex2 done

--- ex2/app.js
+++ ex2/app.js
@@ -1,68 +1,126 @@
 const buttons = document.querySelectorAll("button");
-const display = document.querySelector("#result");
-
-let a = 0;
-let op = null;
-let b = 0;
+const displays = {
+  top: document.getElementById("top"),
+  op: document.getElementById("op"),
+  bottom: document.getElementById("bottom"),
+};
 
 const opDict = {
-    'n': flipSign,
-    'c': clear,
-    '%': () => op = (a, b) => a % b,
-    '/': () => op = (a, b) => a / b,
-    '*': () => op = (a, b) => a * b,
-    '-': () => op = (a, b) => a - b,
-    '+': () => op = (a, b) => a + b,
-    '=': evaluate,
-    '.':
+  n: flipSign,
+  c: clear,
+  "+": addOp("+", (a, b) => a + b),
+  "-": addOp("-", (a, b) => a - b),
+  "*": addOp("×", (a, b) => a * b),
+  "/": addOp("÷", (a, b) => a / b),
+  "%": addOp("%", (a, b) => a % b),
+  ".": addDot,
+  "=": evaluate,
+};
+
+for (let i = 0; i <= 9; i++) {
+  const num = i.toString();
+  opDict[num] = addDigit(num);
+}
+
+function addOp(name, operation) {
+  return (state) => {
+    if (state.op !== null) {
+      state = evaluate(state, false);
+    }
+    return {
+      top: "",
+      op: { name, operation },
+      bottom: state.top === "" ? state.bottom : state.top,
+    };
+  };
 }
 
 function addDigit(num) {
-    if (op == null)
-        a = a * 10 + num;
-    else
-        b = b * 10 + num;
+  return (state) => {
+    const newVal = state.top === "0" ? num : state.top + num;
+    return {
+      top: newVal,
+      op: state.op,
+      bottom: state.bottom,
+    };
+  };
 }
 
-function flipSign() {
-    if (op == null)
-        a = -a;
-    else
-        b = -b;
+function flipSign(state) {
+  if (state.top === "0" || state.top === "")
+    return {
+      top: "0",
+      op: state.op,
+      bottom: state.bottom,
+    };
+  return {
+    top: state.top[0] === "-" ? state.top.substring(1) : "-" + state.top,
+    op: state.op,
+    bottom: state.bottom,
+  };
 }
 
-function clear() {
-    if (op == null)
-        a = 0;
-    else
-        b = 0;
+function clear(state) {
+  return {
+    top: "0",
+    op: state.op,
+    bottom: state.bottom,
+  };
 }
 
-function addDot() {
+function addDot(state) {
+  return {
+    top: state.top.includes(".") ? state.top : state.top + ".",
+    op: state.op,
+    bottom: state.bottom,
+  };
 }
 
-function evaluate() {
-    if (op == null)
-        return;
-    a = op(a, b);
-    b = 0;
-    aDot = false;
-    bDot = false;
-    op = null;
+function evaluate(state, force = true) {
+  if (state.op === null || (!force && state.top === "")) return state;
+  return {
+    top: "",
+    op: null,
+    bottom: state.op.operation(parse(state.bottom), parse(state.top)),
+  };
 }
+
+function parse(numStr) {
+  return numStr === "" ? 0 : Number.parseFloat(numStr);
+}
+
+function strToDisplay(num) {
+  if (num === "") return "0";
+  if (num[0] === ".") return "0" + num;
+  return num;
+}
+
+function toDisplay(state) {
+  const top = strToDisplay(
+    state.top === "" && state.op === null ? state.bottom : state.top
+  );
+  const op = state.op === null ? "" : state.op.name;
+  const bottom = state.op === null ? "&nbsp;" : strToDisplay(state.bottom);
+  return { top, op, bottom };
+}
+
+let state = {
+  top: ``,
+  op: null,
+  bottom: ``,
+};
 
 function onButtonClick(evt) {
-    const button = evt.target;
-    const opC = button.dataset.op;
-    const num = Number.parseInt(opC);
-    if (num >= 0)
-        addDigit(num);
-    else
-        opDict[opC]();
-    display.innerHTML = Math.round((op == null ? a : b) * 1000) / 1000;
+  const button = evt.target;
+  const op = button.dataset.op;
+  state = opDict[op](state);
+  const displayStrings = toDisplay(state);
+  for (const item in displayStrings) {
+    displays[item].innerHTML = displayStrings[item];
+  }
 }
 
 for (const button of buttons) {
-    if (button.dataset.op == undefined) continue;
-    button.addEventListener('click', onButtonClick)
+  if (button.dataset.op == undefined) continue;
+  button.addEventListener("click", onButtonClick);
 }
